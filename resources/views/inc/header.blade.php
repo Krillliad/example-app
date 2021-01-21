81c9d073d5c6c95c7aab22bc64976021bdceac6b
Krillliad <kirill.g@bilimmail.kz>
2021-01-21 17:52:49 +0600

    added form and name routes

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,8 +1,8 @@
 <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <p class="h5 my-0 mr-auto fw-normal">Example App</p>
     <nav class="my-2 my-md-0 me-md-3">
-        <a class="p-2 text-dark" href="/">Главная</a>
-        <a class="p-2 text-dark" href="about">О нас</a>
-        <a class="p-2 text-dark" href="contact">Контакты</a>
+        <a class="p-2 text-dark" href="{{ route('home') }}">Главная</a>
+        <a class="p-2 text-dark" href="{{ route('about') }}">О нас</a>
+        <a class="p-2 text-dark" href="{{ route('contact') }}">Контакты</a>
     </nav>
 </header>
